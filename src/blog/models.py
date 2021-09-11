d9fe2be1648f8be1d89b5bdde70dca22aac675e7
Oytizzo <oytizzo@gmail.com>
2021-09-12 05:28:54 +0600

    Add Post model, register admin and app_name

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    slug = models.SlugField(blank=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
