281b503e5c96bf7a821aa2620ed6a6112874b3d4
Oytizzo <oytizzo@gmail.com>
2021-09-12 09:05:14 +0600

    Slugify Post model with random slug and default author add

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,6 +1,16 @@
+import random
+import string
+
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
+
+DEFAULT_ID = 1
+
+
+def rand_slug():
+    return ''.join(random.choice(string.ascii_letters) for _ in range(6))
 
 
 class Post(models.Model):
@@ -8,7 +18,12 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=DEFAULT_ID)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(rand_slug() + "-" + self.title)
+        super().save(*args, **kwargs)
