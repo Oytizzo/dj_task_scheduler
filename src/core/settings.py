e290bdf67dc3d7ed19175b7bf278024a0a324e1f
Oytizzo <oytizzo@gmail.com>
2021-09-13 01:22:08 +0600

    Install and config django-redis

--- src/core/settings.py
+++ src/core/settings.py
@@ -126,3 +126,16 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# django-redis
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+CACHE_TTL = 60 * 15
